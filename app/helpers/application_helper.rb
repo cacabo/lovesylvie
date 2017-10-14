b425573298c400f6d367fd30f811af3cb7600010
ccabo1 <ccabo@seas.upenn.edu>
2017-10-14 12:35:51 -0400

    application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  # Helper methods for page meta tags and titles
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
+  def keywords(page_keywords)
+    content_for(:keywords) { page_keywords }
+  end
+
+  def image(page_image)
+    content_for(:image) { page_image }
+  end
 end
