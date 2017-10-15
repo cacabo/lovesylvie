6af7cf6f3d0672df5b29ec2ca6aa03ea609350a8
ccabo1 <ccabo@seas.upenn.edu>
2017-10-15 03:10:18 -0400

    routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # get '/register/:id', to: 'registrations#new', as: 'referral'
   get '/users/:id', to: 'users#show', as: 'user'
 
-  get '/users/:id/register', to: 'registrations#new', as: 'referral'
-
   devise_for :users, controllers: {registrations: 'registrations'}, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: '' }
+
+  devise_scope :user do
+    get 'register/:id', to: 'registrations#new', as: 'new_referral'
+    post 'register/:id', to: 'registrations#create', as: 'create_referral'
+  end
 end
