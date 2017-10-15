18ae676f6b336594e3e0f6c06ba8ad1e0e1afbe3
ccabo1 <ccabo@seas.upenn.edu>
2017-10-15 02:58:46 -0400

    got referrals working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,31 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
     if params[:id]
-      puts "ID: " + params[:id]
-      @user = User.find(params[:id])
+      if User.exists?(params[:id])
+        @user = User.find(params[:id])
+        super
+      else
+        redirect_to new_user_registration_path
+      end
+    else
+      super
     end
-    super
   end
 
   def create
     if params[:id]
-      @user = User.find(params(:id))
-      @user.referrals ++
-      @user.save
+      if User.exists?(params[:id])
+        @user = User.find(params[:id])
+        @user = User.find(params[:id])
+        @user.referrals += 1
+        @user.save
+        super
+      else
+        redirect_to new_user_registration_path
+      end
+    else
+      super
     end
-    super
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable :validatable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
-
-  validates :email, uniqueness: true
-  validates :email, presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "Email is invalid"
+         :recoverable, :rememberable, :trackable, :validatable
 end
