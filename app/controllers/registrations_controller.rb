288b72ef0cf17741e61964ad60cb0b44b5401b7b
ccabo1 <ccabo@seas.upenn.edu>
2018-01-31 23:39:10 -0500

    Fixed registration bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   def new
     if params[:id]
       if User.exists?(params[:id])
-        @user = User.find(params[:id])
+        @referrer = User.find(params[:id])
         super
       else
+        flash[:alert] = "Referrer with passed in ID not found"
         redirect_to new_user_registration_path
       end
     else
@@ -12,16 +13,31 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  # def referred
+  #   if params[:id]
+  #     if User.exists?(params[:id])
+  #       @referrer = User.find(params[:id])
+  #     else
+  #       flash[:alert] = "Referrer with passed in ID not found"
+  #       redirect_to new_user_registration_path
+  #       new
+  #     end
+  #   else
+  #     flash[:alert] = "Referrer with passed in ID not found"
+  #     redirect_to new_user_registration_path
+  #     new
+  #   end
+  # end
+
   def create
     if params[:id]
       if User.exists?(params[:id])
-        @user = User.find(params[:id])
         @user = User.find(params[:id])
         @user.referrals += 1
         @user.save
         super
       else
-        redirect_to new_user_registration_path
+        redirect_to new_referral_path(params[:id])
       end
     else
       super
