fbe91acff6a440d0f1fc5021bf7c8e502c5514db
ccabo1 <ccabo@seas.upenn.edu>
2018-02-01 01:56:30 -0500

    working with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set the host
-  config.action_mailer.default_url_options = { host: "www.lovesylvie.com" }
+  config.action_mailer.default_url_options = { host: "https://lovesylvie.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
