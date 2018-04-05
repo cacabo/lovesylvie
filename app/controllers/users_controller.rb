7a2e03227195dba452f9dff526056d9042f4e2b0
ccabo1 <ccabo@seas.upenn.edu>
2018-04-04 21:55:40 -0400

    Better share to twitter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    # If the user is not logged in, they cannot see their profile page
+    # Flash an error and redirect them to home
     if (not current_user) or (current_user != @user) or (not current_user.isAdmin)
       flash[:alert] = "You do not have permissions to view this user's info"
       redirect_to root_path
+    else
+      @url = request.url[0, request.url.index("/users")] + '/register/' + current_user.id.to_s
+      @text = URI.escape("Sign up for Love, Sylvie with my unique link:", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      @encoded = URI.escape(@url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     end
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     if (current_user.present?)
       @url = request.original_url + 'register/' + current_user.id.to_s
+      @text = URI.escape("Sign up for Love, Sylvie with my unique link:", Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
       @encoded = URI.escape(@url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     end
   end
