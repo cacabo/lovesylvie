d654857013fc28000a1f5a992f9195d1dbc9e71f
ccabo1 <ccabo@seas.upenn.edu>
2018-04-04 21:34:38 -0400

    Share via fb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  def index
+    if (current_user.present?)
+      @url = request.original_url + 'register/' + current_user.id.to_s
+      @encoded = URI.escape(@url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    end
+  end
 end
